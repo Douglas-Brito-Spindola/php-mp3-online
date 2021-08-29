ac3d1af80762a6e39e0896b18d4eaf33073608c5
Douglas Spíndola <douglas.spindola15@gmail.com>
2021-08-29 01:18:00 -0300

    criado pasta album, pasta pages e deletado arquivos de license e readme

---
+++ pages/albums.php
@@ -0,0 +1,26 @@
+<h1>Álbum</h1>
+
+<a href="?page=new_album" class="btn btn-success">Adicionar Novo Album</a>
+<hr>
+<div class="row">
+
+    <?php
+    $albums = getAlbums();
+
+    foreach( $albums as $album):
+
+        $info_album = explode('/', $album);
+        $nome_album = $info_album[1];
+
+        $imagem_album = "{$album}/{$nome_album}.jpg";
+    ?>
+    
+    <div class="col-3" class="album">
+        <a href="?page=musics&algum=<?=$nome_album?>">
+            <img src="<?=$imagem_album?>" alt="<?=$nome_album?>" class="img-album"> 
+            <h4><?=$nome_album?></h4>
+        </a> 
+    </div>
+
+    <?php endforeach?>
+</div>
---
+++ pages/new_album.php
@@ -0,0 +1,16 @@
+<a href="?pge=albums">Voltar</a>
+
+<h1>Cadastrar Novo Album</h1>
+<form action="#" method="post">
+    <div class="form-group">
+        <input type="text" name="name" placeholder="Nome:" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <input type="file" name="imagem_album" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <button type="submit" class="btn btn-success">Cadastrar</button>
+    </div>
+</form>
