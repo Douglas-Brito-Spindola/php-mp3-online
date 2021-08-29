21aa38250b01ff3a4c0bc5c2d2089a29f317af65
Douglas Spíndola <douglas.spindola15@gmail.com>
2021-08-29 02:05:23 -0300

    adicionando nome do algum no titulo da pagina, pegando da url o nome e passando o nome no paremeto do metodo listar musicas

---
+++ pages/musics.php
@@ -0,0 +1,18 @@
+<a href="?page=albums"> Voltar para os Albums </a>
+<h1> Musicas do Album <?=$_GET['album']?></h1>
+<hr>
+<?php 
+ $album = $_GET['album'];
+ $musics = getMusics($album);
+
+ foreach($musics as $music):
+
+?>
+
+<div class="col-12">
+    <audio src="<?=$music?>" controls></audio>
+</div>
+
+<?php 
+endforeach 
+?>
